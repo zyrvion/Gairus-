687ea0e70abaeebeb280a8ea7456cdc5a92d3b89
zyrvion <zyrvion@gmail.com>
2026-09-17 22:20:46 +0000

    Corriger cache interface Gairus

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'gairus-v4';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+
+  if (url.pathname === '/' ||
+      url.pathname.startsWith('/static/') ||
+      url.pathname === '/manifest.json') {
+    event.respondWith(fetch(event.request));
+  }
+});
